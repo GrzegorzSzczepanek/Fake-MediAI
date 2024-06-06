95618e49e1e6763478d8e661e934b62341f298b1
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2024-06-06 02:23:57 +0200

    base app

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,16 @@
 <script>
-    import { onMount } from 'svelte';
-    import { data, mlData, fetchData, fetchMl } from '../stores/data';
-	import Post from '$lib/components/Post.svelte';
+  import { onMount } from 'svelte';
+  import { data, mlData, fetchData, fetchMl } from '../stores/data';
 
-    onMount(() => {
-        fetchData();
-        fetchMl();
-    });
+  onMount(() => {
+      fetchData();
+      fetchMl();
+  });
 </script>
 
 <main>
-    <h1>Hello SvelteKit with Flask!</h1>
-    <p>{$data ? $data.message : 'Loading...'}</p>
-    <h2>ML Data</h2>
-    <p>{$mlData ? $mlData.message : 'Loading ML data...'}</p>
-    <Post username="Grzegina" content="Hello SvelteKit with Flask!" likes={123} comments={123} />
+  <h1>Hello SvelteKit with Flask!</h1>
+  <p>{$data ? $data.message : 'Loading...'}</p>
+  <h2>ML Data</h2>
+  <p>{$mlData ? $mlData.message : 'Loading ML data...'}</p>
 </main>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    './src/**/*.{html,js,svelte,ts}',
+    './src/**/*.{html,js,svelte,ts}'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
